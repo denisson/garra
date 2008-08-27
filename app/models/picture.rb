05a60a06d40af57572444fbebc30686dd77fd906
denisson <dap.tci@gmail.com>
2008-08-27 17:34:54 -0300

    commit inicial

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,11 @@
 class Picture < ActiveRecord::Base
-	has_attachment :content_type => :image,:storage => :file_system, :resize_to=> [50,50]
+
+    has_attachment  :processor => 'MiniMagick',
+                    :content_type => :image, 
+                    :storage => :file_system, 
+                    :max_size => 500.kilobytes,
+                    :resize_to => '320x200>',
+                    :thumbnails => { :thumb => '10x10>' }
+
+	validates_as_attachment
 end
